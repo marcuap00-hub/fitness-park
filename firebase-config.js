51f5db09ad13512f9957b4f367906fb406cb32cf
marcuap00-hub <marcu.ap00@gmail.com>
2026-09-16 16:57:39 +0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// Configurația proiectului Firebase pentru Fitness Park.
+const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyB-Mz-XXMZ9iLfEjh5k8rqbc90P60qIEMM",
+  authDomain: "fitness-park-9e224.firebaseapp.com",
+  projectId: "fitness-park-9e224",
+  storageBucket: "fitness-park-9e224.firebasestorage.app",
+  messagingSenderId: "531088923349",
+  appId: "1:531088923349:web:c3fd1a49d035efa4d6605c"
+};
+
